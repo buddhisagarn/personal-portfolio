ab44396a0d261ea95e5dd254870dab29723703ba
Buddhi Sagar Nyaupane <rabinyaupane55@gmail.com>
2024-11-30 10:31:38 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Buddhi Sagar Nyaupane</h1>
+      <div className="card">
+       
+        <p>
+         This is my personal portfolio website.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
