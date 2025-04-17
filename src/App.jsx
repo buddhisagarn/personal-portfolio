8be7c2628fc60346f57188ec0caf782cb99b9595
student <[rabinyaupane55@gmail.com]>
2025-04-17 17:18:43 +0545

    lets go

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="laptop">
+        <div className="screen">
+          <div className="header"></div>
+          <div className="text">Buddhi Sagar Nyaupane</div>
+        </div>
+        <div className="keyboard"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
