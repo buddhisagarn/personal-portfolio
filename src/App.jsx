71f39603878e49e1bb7ce8707b27187b61b386c9
student <[rabinyaupane55@gmail.com]>
2025-04-21 10:11:03 +0545

    Eroor Footer.jax

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import Bsection1 from "./components/Body/Bsection1.jsx";
 import Bsection2 from "./components/Body/Bsection2.jsx";
-import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 function App() {
   return (
     <>
