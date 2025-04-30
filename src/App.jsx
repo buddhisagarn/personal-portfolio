9473c36d13c847d68744665fe467ca2bd5a8bce0
student <[rabinyaupane55@gmail.com]>
2025-04-30 15:01:40 +0545

    error related path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./App.css";
-import Nav from "./components/Header/nav";
+import Nav from "././components/Header/Nav.jsx";
+
 // import Head from "./components/profile/Head.jsx";
 function App() {
   return (
