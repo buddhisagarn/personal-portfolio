36913b38bc04b0884d2c90bd69fd64953afab0d4
student <[rabinyaupane55@gmail.com]>
2025-05-13 15:33:45 +0545

    mobile responsive

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -9,19 +9,21 @@ import Search from "./Search";
 export default function Nav() {
   return (
     <div className="nav">
-      <div className="left-nav">
-        <Menu />
-        <Search />
-      </div>
-      <div className="right-nav">
-        <Home />
-        <Globe />
-        <Linkedin />
-        <Mail />
-        <Message />
-      </div>
-      <div className="last-nav">
-        <i className="fa-solid fa-microphone"></i>
+      <div className="nav-in">
+        <div className="left-nav">
+          <Menu />
+          <Search />
+        </div>
+        <div className="right-nav">
+          <Home />
+          <Globe />
+          <Linkedin />
+          <Mail />
+          <Message />
+        </div>
+        <div className="last-nav">
+          <i className="fa-solid fa-microphone"></i>
+        </div>
       </div>
     </div>
   );
